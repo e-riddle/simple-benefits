101f50e5fcfd2c6a1f9626a165b632380b48f12d
Eric Riddle <e-riddle@hotmail.com>
2019-01-06 14:37:14 -0500

    fix bearer token

--- src/app/health-benefits-calc-api.service.ts
+++ src/app/health-benefits-calc-api.service.ts
@@ -6,24 +6,26 @@ import { catchError, map } from 'rxjs/operators';
 import { IHealthBenefitsCalc } from './health-benefits-calc.interface';
 import { environment } from '../environments/environment';
 
-const token = localStorage.getItem('jwt');
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json',
-    'Authorization': 'Bearer ' + token
-  })
-};
-
 @Injectable({
   providedIn: 'root'
 })
 export class HealthBenefitsCalcApiService implements IHealthBenefitsCalc {
   BASE_URL: string = environment.apiBaseUrl;
-  constructor(private http: HttpClient) {}
+  HTTP_OPTIONS: any;
+
+  constructor(private http: HttpClient) {
+    const token = localStorage.getItem('jwt');
+
+    this.HTTP_OPTIONS = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + token
+      })
+    };
+  }
 
   calcBenefits(parameters: BenefitsCalcParameters): Observable<number> {
-    return this.http.post<BenefitsCalcParameters>(`${this.BASE_URL}/BenefitsCalc`, parameters, httpOptions)
+    return this.http.post<BenefitsCalcParameters>(`${this.BASE_URL}/BenefitsCalc`, parameters, this.HTTP_OPTIONS)
       .pipe(
         map((response: any) => {
           return response;
